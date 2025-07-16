15b5553817b9984f750b4f1cfbaaac860390143b
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2025-07-16 16:59:37 -0500

    chore: removes unused imports

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,47 @@
+import EmailTemplate from "../../../../emails/consult-request";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req: Request) {
+	const {
+		firstName,
+		lastName,
+		email,
+		phone,
+		projectType,
+		roomType,
+		projectSize,
+		timeline,
+		budget,
+		message,
+	} = await req.json();
+
+	try {
+		const { data, error } = await resend.emails.send({
+			from: "Cruz Remodeling Website <website@cruzremodeling.com>",
+			to: ["gideon.ibemere.jr@gmail.com", "gracie.ibemere@gmail.com"],
+			subject: "New Consultation Request",
+			react: EmailTemplate({
+				firstName,
+				lastName,
+				email,
+				phone,
+				projectType,
+				roomType,
+				projectSize,
+				timeline,
+				budget,
+				message,
+			}),
+		});
+
+		if (error) {
+			return Response.json({ error }, { status: 500 });
+		}
+
+		return Response.json(data);
+	} catch (error) {
+		return Response.json({ error }, { status: 500 });
+	}
+}
